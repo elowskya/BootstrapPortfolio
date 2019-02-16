caa1da7b040d11cf8c6dc21a16210c75273d7599
unknown <ashley.elowsky123@gmail.com>
2019-02-15 17:28:19 -0800

    added new project to portfolio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,5 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// });
+});
 
 
-nav-item nav-link
-
-function navigation(event, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
